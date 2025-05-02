1685a23a480ac8157f116848d9f6482436f43e9e
fiegus <annafiega@gmail.com>
2025-05-02 20:50:50 +0200

    error + retry added

--- src/features/medals/Medals.tsx
+++ src/features/medals/Medals.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import Link from '@mui/joy/Link'
 import { useMedalsQuery } from './api'
 import { MedalTable } from './MedalTable'
 
 export const Medals = () => {
-  const { isLoading, isError, error, data } = useMedalsQuery()
+  const { isLoading, isError, data, refetch } = useMedalsQuery()
 
   if (isLoading) {
     return <div>Loading ...</div>
   }
 
   if (isError) {
-    return <div>error.message</div>
+    return (
+      <div>
+        Can't get medal data.
+        <Link component="button" onClick={() => refetch}>
+          Try again.
+        </Link>
+      </div>
+    )
   }
 
   return <MedalTable medals={data || []} />
--- src/features/medals/api.ts
+++ src/features/medals/api.ts
@@ -1,21 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query'
 
 export interface CountryMedals {
-  code: string;
-  gold: number;
-  silver: number;
-  bronze: number;
+  code: string
+  gold: number
+  silver: number
+  bronze: number
 }
 
-const GET_MEDALS_QUERY_KEY = "GET_MEDALS_QUERY_KEY";
+const GET_MEDALS_QUERY_KEY = 'GET_MEDALS_QUERY_KEY'
 
 async function fetchMedals(): Promise<CountryMedals[]> {
-  const res = await fetch("/data/medals.json");
+  const res = await fetch('/data/medals.json')
   if (!res.ok) {
-    throw new Error("Failed to fetch medals data");
+    throw new Error('Failed to fetch medals data')
   }
-  return res.json();
+  return res.json()
 }
 
 export const useMedalsQuery = () =>
-  useQuery({ queryKey: [GET_MEDALS_QUERY_KEY], queryFn: fetchMedals });
+  useQuery({ queryKey: [GET_MEDALS_QUERY_KEY], queryFn: fetchMedals })
